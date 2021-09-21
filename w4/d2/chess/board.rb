2332e0247995daba55b2ccac0812c8111e9240f1
ntademaru <ntademaru@gmail.com>
2021-09-21 14:56:17 -0700

    defined positions

--- w4/d2/chess/board.rb
+++ w4/d2/chess/board.rb
@@ -5,4 +5,19 @@ class Board
     @sentinel
   end
 
+  def [](pos)
+    row,col = pos
+    @board[row][col]
+  end
+
+  def []=(pos,val)
+    row,col = pos
+    @board[row][col] = val
+  end
+
+  def valid_pos?(pos)
+    row,col = pos
+    row.between?(0,7) && col.between?(0,7)
+  end
+
 end
