603fc7e28fa5499255de212a35423434b1012ad8
ntademaru <ntademaru@gmail.com>
2021-09-21 14:20:24 -0700

    Additions to manager.rb

--- w4/d2/manager.rb
+++ w4/d2/manager.rb
@@ -1,9 +1,15 @@
 require_relative "employee.rb"
 
 class Manager < Employee 
+  attr_reader :employees
+
   def initialize
     super
     @employees = []
+  end
+
+  def add_employees(sub)
+    employees << sub
   end
 
   def bonus(multiplier)
@@ -16,4 +22,9 @@ class Manager < Employee
     salaries.inject { |acc, el| acc + el }
   end
 end
+
+ned = Manager.new("Ned",1_000_000, "Founder", nil)
+darren = Manager.new("Darren",78_000,"TA Manager", ned)
+shawna = Employee.new("Shawna",12_000,"TA",darren)
+david = Employee.new("David",10_000,"TA",darren)
 
