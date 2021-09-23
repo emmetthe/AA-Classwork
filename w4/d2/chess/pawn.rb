a974ce6363aa9f478697af3792c6ce6b05eff66b
Emmett He <emmetthe26@gmail.com>
2021-09-22 17:31:09 -0700

    Made pawn class

--- w4/d2/chess/pawn.rb
+++ w4/d2/chess/pawn.rb
@@ -2,30 +2,47 @@ require_relative 'piece'
 
 class Pawn < Piece
 
-  def symbol
+  attr_reader :color, :board
+  attr_accessor :pos
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+  end
 
+  def symbol
+    :P
   end
 
   def move_dirs
+    if at_start_row?
+      forward_steps
+    elsif at_start_row?
 
+    end
   end
 
   private
   
   def at_start_row?
-
+    true
   end
 
   def forward_dir
-
+    row, col = pos
+    new_pos = [row, col + 1]
+    if @board.valid_pos?(new_pos)
+      if @board[new_pos].is_a?(NullPiece)
+      end
+    end
   end
 
   def forward_steps
-
+    [[0, 1], [0, 2]]
   end
 
   def side_attacks
-    
+    [[1, 1], [-1, 1]]
   end
 
   
