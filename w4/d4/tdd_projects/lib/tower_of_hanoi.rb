720d27b17111e31931c4a70d8dffef5ddedff9cb
Emmett He <emmetthe26@gmail.com>
2021-09-23 16:44:13 -0700

    Add test for empty array in move

--- w4/d4/tdd_projects/lib/tower_of_hanoi.rb
+++ w4/d4/tdd_projects/lib/tower_of_hanoi.rb
@@ -1,6 +1,27 @@
 
 class TowerOfHanoi
 
+  attr_reader :board
 
+  def initialize
+    @board = Array.new(3) {Array.new(3)}
+  end
+ 
+  def move(start_pos, end_pos)
+    if board[end_pos].empty?
+      board[end_pos] << board[start_pos].pop
+    end
+    if board[end_pos].last > board[start_pos].last
+      board[end_pos].unshift(board[start_pos].shift)
+    else
+      raise "can not move there"
+    end
+  end
 
 end
+
+# [
+#   [5, 4, 3, 2   ]
+#   [1            ]
+#   [             ]
+# ]
