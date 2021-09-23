2cf0786ee3849b41c1565fef0d1e2253c709b505
thiago <17tawi@gmail.com>
2021-09-23 16:19:44 -0700

    find a way to check valid positions

--- w4/d4/tdd_projects/spec/tower_of_hanoi_spec.rb
+++ w4/d4/tdd_projects/spec/tower_of_hanoi_spec.rb
@@ -1,1 +1,13 @@
 require 'tower_of_hanoi'
+
+describe TowerOfHanoi do 
+    subject(:towers){TowerOfHanoi.new}
+       describe "#move" do
+            it "can move to an empty array" do
+                expect{towers.move(0, 1)}.to_not raise_error 
+            end
+            it "can only move to a larger disc" do
+                expect(towers.move)
+            end
+        end
+end
