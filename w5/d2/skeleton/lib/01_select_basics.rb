275c50f213fcd6cee86e5b153c1e8e616c6c5b06
Emmett He <emmetthe26@gmail.com>
2021-09-28 15:29:07 -0700

    finished part 2 and 3

--- w5/d2/skeleton/lib/01_select_basics.rb
+++ w5/d2/skeleton/lib/01_select_basics.rb
@@ -88,5 +88,11 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+  SELECT
+    name, area / 1000 AS th_sq_kilometers
+  FROM
+    countries
+  WHERE
+    area BETWEEN 200000 AND 250000
   SQL
 end
