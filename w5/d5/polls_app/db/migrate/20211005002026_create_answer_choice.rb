70f4843084b8a02ccff7fabf33f8ee6eefc94fb1
Emmett He <emmetthe26@gmail.com>
2021-10-04 17:25:30 -0700

    Created database and migrations

---
+++ w5/d5/polls_app/db/migrate/20211005002026_create_answer_choice.rb
@@ -0,0 +1,11 @@
+class CreateAnswerChoice < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answer_choices do |t|
+      t.text :text, null: false
+      t.integer :question_id, null: false
+
+      t.timestamps
+    end
+    add_index :answer_choices, :question_id
+  end
+end
