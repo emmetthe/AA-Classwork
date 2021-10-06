b5185ae540e44f6a95a302660289eb27a5f353c4
Emmett He <emmetthe26@gmail.com>
2021-10-05 17:31:36 -0700

    made initialize method

---
+++ w6/d2/skeleton/lib/01_sql_object.rb
@@ -0,0 +1,81 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.name.underscore.pluralize
+  end
+
+  def self.all
+   
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    params.each do |k, v|
+      col = k.to_sym
+      if self.class.columns.include?(col)
+        self.send("#{col}=", v)
+      else
+        raise "unknown attribute '#{col}'"
+      end
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    self.class.columns.map { |attr| self.send(attr) }
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
