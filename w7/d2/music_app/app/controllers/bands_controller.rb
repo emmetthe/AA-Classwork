5e69d64d5019fbcfee81bb4767eae380a761a95d
Emmett He <emmetthe26@gmail.com>
2021-10-12 17:32:48 -0700

    Finish bands controller

--- w7/d2/music_app/app/controllers/bands_controller.rb
+++ w7/d2/music_app/app/controllers/bands_controller.rb
@@ -4,12 +4,43 @@ class BandsController < ApplicationController
     @bands = Band.all
     render :index
   end
+  
+  def new
+    @band = Band.new
+    render :new
+  end
 
   def create
     band = Band.new(band_params)
-    
+    if band.save
+      redirect_to band_url(band)
+    else
+      flash.now[:errors] = band.errors.full_messages
+      render :new
+    end
   end
 
+  def edit
+    @bands = Band.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @band = Band.find(params[:id])
+    if @band.update(band_params)
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    redirect_to bands_url
+  end
+
+  private
   def band_params
     params.require(:band).permit(:name)
   end
