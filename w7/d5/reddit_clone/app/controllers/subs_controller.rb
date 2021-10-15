0edb16db0055b51030f5ef9e957c35014dd1ebe2
Chris <chris.cheasty3@gmail.com>
2021-10-15 15:09:23 -0700

    Add subs model and controller

--- w7/d5/reddit_clone/app/controllers/subs_controller.rb
+++ w7/d5/reddit_clone/app/controllers/subs_controller.rb
@@ -1,2 +1,52 @@
 class SubsController < ApplicationController
+
+  before_action :require_login, except: [:index, :show]
+
+  def new
+    @sub = Sub.new
+    render :new
+  end
+
+  def index
+    @subs = Sub.all 
+    render :index 
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+    render :show
+  end
+
+  def create 
+    @sub = Sub.new(sub_params)
+    @sub.mod_id = params[:user_id]
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new 
+    end
+  end
+
+  def edit
+    @sub = Sub.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sub = Sub.find(params[:id])
+    
+    if @sub.update(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash[:errors] = @sub.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def sub_params 
+    params.require(:sub).permit(:title, :description)
+  end
 end
