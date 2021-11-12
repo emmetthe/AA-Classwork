8c014f5fc6b87aa425c0c149c00d090b7adfb960
Emmett He <emmetthe26@gmail.com>
2021-11-11 17:06:26 -0800

    day 4 edits on todo list

---
+++ w11/d2/todolist/TodoList/app/controllers/api/todos_controller.rb
@@ -0,0 +1,40 @@
+class Api::TodosController < ApplicationController
+  def index
+    todos = Todo.all 
+    render json: todos
+  end
+
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    todo = Todo.find_by(id: params[:id])
+    if todo && todo.update
+      render json: todo
+    else
+      render json: todo.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    todo = Todo.find_by(id: params[:id])
+    if todo
+      todo.delete
+    end
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :body, :done)
+  end
+
+end
---
+++ w11/d2/todolist/TodoList/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root"
+  namespace :api, default: {format: :json} do
+    resources :todos, only: [:index, :show, :create, :update, :destroy]
+  end
+end
