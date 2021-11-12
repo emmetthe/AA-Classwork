a92a5c723f824c76d17fa6f6f191448c39d93785
Emmett He <emmetthe26@gmail.com>
2021-11-12 14:10:31 -0800

    made thunk

--- w11/d5/skeleton/frontend/pokedex.jsx
+++ w11/d5/skeleton/frontend/pokedex.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as API_Util from './util/api_util';
 import { receiveAllPokemon } from './actions/pokemon_actions'
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 
-
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.fetchAllPokemon = API_Util.fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
 });
---
+++ w11/d5/skeleton/frontend/store/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux"
+import rootReducer from "../reducers/root_reducer"
+import { logger } from 'redux-logger'
+
+const configureStore = () => (
+  createStore(rootReducer, applyMiddleware(logger))
+)
+
+export default configureStore;
