1b4e636421eb2de0c2d906ba3612818c6c38249d
Emmett He <emmetthe26@gmail.com>
2021-11-16 14:34:32 -0800

    started controller

--- w12/d2/benchbnb/app/controllers/application_controller.rb
+++ w12/d2/benchbnb/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method:
+
+  def login!(user)
+    session[:session_token] = user.session_token
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+
+
 end
