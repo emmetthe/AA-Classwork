be6e54188d08b77c89ce79d4eb9310435ed0e23b
Emmett He <emmetthe26@gmail.com>
2021-11-16 17:27:50 -0800

    started sessions errors reducer

---
+++ w12/d2/benchbnb/frontend/actions/session_actions.js
@@ -0,0 +1,31 @@
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+})
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+})
+
+const receiveErrors = (errors) => ([
+  errors
+])
+
+export const login = (user) => (dispatch) => (
+  logIn(user)
+    .then(user =>  dispatch(receiveCurrentUser(user)))
+)
+
+export const logout = () => (dispatch) => (
+  logOut()
+    .then(()=>  dispatch(logoutCurrentUser()))
+)
+
+export const signup = (user) => (dispatch) => (
+  signUp(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+)
---
+++ w12/d2/benchbnb/frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
+
+const _nullSession = {
+  currentUser: null
+}
+
+export default (state={}, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, action.currentUser)
+    case LOGOUT_CURRENT_USER:
+      return _nullSession;
+    default:
+      return state;
+  }
+}
